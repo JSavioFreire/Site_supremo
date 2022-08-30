b4a7919c0abafd52ad5267aa1b8c7e3cbd5a80e6
Sávio Freire <freiresavio7@gmail.com>
2022-08-30 10:20:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $(function(){
     var praia = $('.header2 .praia');
     var menupraia = $('main #painel .praia2')
-   
+    abrirmenu();
+
     function abrirmenu(){
         praia.hover(function(){
             menupraia.fadeIn();
